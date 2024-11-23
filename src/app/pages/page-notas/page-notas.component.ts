1788567792548ede7f13baa9635f11ba02a5759b
Mike <miguel.garcia.garcia1201@gmail.com>
2024-11-22 19:59:17 -0600

    Sidebar

--- src/app/pages/page-notas/page-notas.component.ts
+++ src/app/pages/page-notas/page-notas.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-page-notas',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './page-notas.component.html',
-  styleUrl: './page-notas.component.css'
+  styleUrl: './page-notas.component.css',
 })
-export class PageNotasComponent {
-
-}
+export class PageNotasComponent {}
