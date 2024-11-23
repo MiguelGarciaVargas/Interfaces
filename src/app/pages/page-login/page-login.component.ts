08c54a567b5f3936e84d9ecfad9b343646762dee
Mike <miguel.garcia.garcia1201@gmail.com>
2024-11-23 13:56:42 -0600

    Login page

--- src/app/pages/page-login/page-login.component.ts
+++ src/app/pages/page-login/page-login.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-page-login',
   standalone: true,
-  imports: [],
+  imports: [LoginFormComponent],
   templateUrl: './page-login.component.html',
-  styleUrl: './page-login.component.css'
+  styleUrl: './page-login.component.css',
 })
-export class PageLoginComponent {
-
-}
+export class PageLoginComponent {}
