8e01de22525b89e87ba20267da4d181a61a2b7ee
Daphish <ivan.davila.2003@gmail.com>
2024-11-24 04:20:55 -0600

    Horario de cursos en marcha

--- src/app/pages/page-cursos/page-cursos.component.ts
+++ src/app/pages/page-cursos/page-cursos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { CardRowComponent } from "../../components/card-row/card-row.component";
+import { HorarioComponent } from "../../components/horario/horario.component";
 
 @Component({
   selector: 'app-page-cursos',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, CardRowComponent, HorarioComponent],
   templateUrl: './page-cursos.component.html',
   styleUrl: './page-cursos.component.css'
 })
